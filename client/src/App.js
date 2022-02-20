947becaf651ed04561cf7808fe8235222b29078f
cristobalpina <mondmaxy@gmail.com>
2022-02-19 21:38:43 -0300

    create post from client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import PostCreate from "./PostCreate";
+
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
 };
 
 export default App;
--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,11 @@
+const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
