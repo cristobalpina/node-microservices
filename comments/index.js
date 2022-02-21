d3745991a1cc6d577c8fca450bf95ea86396eea1
cristobalpina <mondmaxy@gmail.com>
2022-02-21 08:41:40 -0300

    update client to display conditional comment messages

--- comments/index.js
+++ comments/index.js
@@ -18,25 +18,46 @@ app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
+  const newComment = { id: commentId, content, status: "pending" };
+
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push(newComment);
 
   commentsByPostId[req.params.id] = comments;
 
   await axios.post("http://localhost:4005/events", {
     type: "CommentCreated",
     data: {
-      id: commentId,
-      content,
+      ...newComment,
       postId: req.params.id,
     },
   });
+
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
-  console.log("Recieved Event:", req.body.type);
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+  console.log("Recieved Event:", type);
+
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => comment.id === id);
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        content,
+        postId,
+        status,
+      },
+    });
+  }
 
   res.send({});
 });
